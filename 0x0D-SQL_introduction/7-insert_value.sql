0fcda04a58f8ac9bbcca99f249712f3b64fd14eb
samkiyya <samuelkiyya2012@gmail.com>
2023-07-18 19:56:34 +0300

    error fixed

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,4 @@
 --Insert into
 INSERT INTO hbtn_0c_0.first_table (id, `name`) VALUES(89, "Best School");
+INSERT INTO hbtn_0c_0.first_table (id, `name`) VALUES(89, "Best School");
+INSERT INTO hbtn_0c_0.first_table (id, `name`) VALUES(89, "Best School");
