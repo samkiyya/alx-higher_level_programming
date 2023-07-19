0de50d937a9343ac35673fdfc46bd202e8305e51
Samuel Aberra <122904898+samkiyya@users.noreply.github.com>
2023-07-19 22:14:02 +0300

    error solved

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,5 @@
+-- script that creates the MySQL server user user_0d_1 and granting privileges
 -- Create a user user_0d_1 with the password user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES 
-ON *.*
-TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+-- Granting all privilages to the user user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
