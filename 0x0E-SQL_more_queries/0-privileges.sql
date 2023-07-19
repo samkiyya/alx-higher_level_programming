2734be7f6b522cee3b1e71d5caa99081363fe1e0
Samuel Aberra <122904898+samkiyya@users.noreply.github.com>
2023-07-19 21:03:35 +0300

    updated

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,9 +1,3 @@
--- Create  the user user_0d_1 if it is not existed
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
---grant privilages to this user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
--- Create  the user user_0d_2 if it is not existed
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 
 -- Lists all privileges of the user user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
